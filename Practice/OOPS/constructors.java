fc59af6f38a602a82d2838bc6efe629e4e31465e
Yash Tiwari <yasht8516@gmail.com>
2026-08-29 19:03:18 +0530

    Add base and derived classes with constructors, getters, setters, and display methods

---
+++ Practice/OOPS/constructors.java
@@ -0,0 +1,95 @@
+class base{
+    int id;
+    String name;
+
+    public base(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public base(){
+        id = 0;
+        name = "your name here";
+    }
+
+    public int setid(int n){
+        return id = n;
+    }
+
+    public String setname(String n){
+        return name = n;
+    }
+
+    public int getid(){
+        return id;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void displayDetails(){
+        System.out.println("\nname: " + name);
+        System.out.println("id: " + id);
+    }
+
+    }
+
+class derived extends base{
+    int age;
+    int salary;
+
+    public derived(String name, int id, int age, int salary){
+        super(id, name);
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public derived(){
+        age = 0;
+        salary = 0;
+    }
+
+    public int setage(int n){
+        return age = n;
+    }
+    
+    public int setsalary(int n){
+        return salary = n;
+    }
+
+    public int getage(){
+        return age;
+    }
+
+    public int getsalary(){
+        return salary;
+    }
+
+    public void displayDetails(){
+        super.displayDetails();
+        System.out.println("age: " + age);
+        System.out.println("salary: " + salary);
+    }
+
+
+
+}
+
+class constructors{
+    public static void main(String[] args) {
+        derived d1 = new derived();
+        d1.setname("Yash");
+        d1.setage(21);
+        d1.setid(101);
+        d1.setsalary(21000);
+
+        System.out.println(d1.getname());
+        System.out.println(d1.getage());
+        System.out.println(d1.getid());
+        System.out.println(d1.getsalary());
+
+        derived d2 = new derived("Harsh", 101, 21, 22000);
+        d2.displayDetails();
+    }
+}
